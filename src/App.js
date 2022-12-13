05c0182bd5a3018775579148e199a3ade7a04cd4
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2022-12-13 20:41:37 -0300

    lint

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/trybetunes" component={Login} />
-        <Route path="/search" component={Search} />
-        <Route path="/album/:id" component={Album} />
-        <Route path="/favorites" component={Favorites} />
-        <Route exact path="/profile" component={Profile} />
-        <Route path="/profile/edit" component={ProfileEdit} />
+        <Route exact path="/trybetunes" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
         <Route path="*" component={ NotFound } />
       </BrowserRouter>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
